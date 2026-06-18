297298916486a4bcfaa6156adfcbe8e5343f14ed
hyangun617 <80374772+hyangun617@users.noreply.github.com>
2026-06-19 08:50:34 +0900

    가장 처음 브랜치

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem; // 인풋 시스템을 사용하기 위해 네임 스페이스를 읽어옴.
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;               // 움직임을 위해 물리연산자를 담을 변수.
+    private Vector2 moveInput;       // 이동 방향.
+
+    private InputSystem inputSystem;
+
+    // 이동 속도
+    public float moveSpeed;
+
+    private void Awake()
+    {
+        // GameObject에 등록된 Rigidbody 컴포넌트를 받아옴.
+        rb = GetComponent<Rigidbody>();
+        inputSystem = new InputSystem();
+
+        // 기본 이동 속도.
+        moveSpeed = 10f;
+    }
+
+    // 해당 플레이어가 활성화 되었을 때.
+    private void OnEnable()
+    {
+        inputSystem.Player.Enable();
+        inputSystem.Player.Move.performed += onMove;
+        inputSystem.Player.Move.canceled += onMove;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // 입력 값에 따라 단위 벡터를 받아옴.
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+
+        rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);        
+    }
+
+    // 플레이어 오브젝트가 비활성화 일 경우 입력도 비활성화.
+    private void OnDisable()
+    {
+        inputSystem.Player.Move.performed -= onMove;
+        inputSystem.Player.Move.canceled -= onMove;
+        inputSystem.Player.Disable();
+    }
+
+    void onMove(InputAction.CallbackContext ctx)
+    {
+        moveInput = ctx.ReadValue<Vector2>();
+    }
+}
---
+++ Assets/Script/CameraManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // 플레이어의 오브젝트를 담을 변수
+    [SerializeField] private GameObject player;
+
+    // 카메라가 플레이어로 부터 떨어질 오프셋
+    public Vector3 offset;
+
+    // 카메라가 아래를 바라보는 정도.
+    // Quaternion = 회전값을 담기 위한 변수.
+    public Quaternion Rotation;
+
+    private void Awake()
+    {
+        // player 할당
+        // FindWithTag를 이용해 Player 태그를 단 게임 오브젝트를 찾음.
+        player = GameObject.FindWithTag("Player");
+
+        // 회전 초기값 지정.
+        Rotation = Quaternion.Euler(60f, 0f, 0f);
+        // 오프셋 초기값 지정
+        offset = new Vector3(0f, 10f, -1f);
+    }
+
+    private void LateUpdate()
+    {
+        // 플레이어의 위치로 카메라 이동
+        transform.position = player.transform.position + offset;
+
+        // 카메라 회전.
+        transform.rotation = Rotation;
+    }
+
+}
